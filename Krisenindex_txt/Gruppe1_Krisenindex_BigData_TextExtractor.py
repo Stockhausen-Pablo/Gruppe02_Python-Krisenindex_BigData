e5366506a41725f166e0a4317a973904128b5b25
Pablo <46866005+Stockhausen-Pablo@users.noreply.github.com>
2020-04-26 13:59:47 +0200

    Update Gruppe1_Krisenindex_BigData_TextExtractor.py
    Added string to txt

--- Krisenindex_txt/Gruppe1_Krisenindex_BigData_TextExtractor.py
+++ Krisenindex_txt/Gruppe1_Krisenindex_BigData_TextExtractor.py
@@ -18,4 +18,13 @@ Created on Sun Apr 26 12:34:00 2020
 # %%
 #string to txt
 
+#Platzhalter für die späteren Artikeltexte
+Artikeltexte = ['aba', 'xyz', 'xgx', 'dssd', 'sdjh']
+
+for x in range(0,len(Artikeltexte)):
+       filename = "Artikeltext%d.txt" % (x)
+       writeFile = open(filename, 'w')
+       writeFile.write(Artikeltexte[x])
+       writeFile.close()
+
 # %%
