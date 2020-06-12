dd5e67cb3e584548285026f28d5a323c2e542b04
PSaalfrank <56958842+PSaalfrank@users.noreply.github.com>
2020-06-12 22:19:46 +0200

    Add files via upload

--- Krisenindex_txt/Gruppe1_Krisenindex_BigData_TextProcessing.py
+++ Krisenindex_txt/Gruppe1_Krisenindex_BigData_TextProcessing.py
@@ -3,22 +3,20 @@
 """
 Created on Mon Jun  1 10:06:02 2020
 
-@author: Paulo
+@author: Gruppe 02
 """
 
+# DATEN EINLESEN
+
 import os
-import collections
+from collections import Counter
+from itertools import chain
+import pandas as pd
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-Word_Bag = []
-Anzahl_Bag = []
-Bin_Bag = []
-all_text_liste_k = []
-all_text_liste_n = []
-all_text_liste_c = []
-
 
 def einlesen(x):
+    all_text_liste = []
     if x == 'k':
         folder_path = dir_path + '/Krise'
     elif x == 'n':
@@ -31,114 +29,123 @@ def einlesen(x):
         files = os.listdir(path)
         for file in sorted(files):
             fullpath = path + '/' + file
-            with open(fullpath, 'r', encoding="utf8") as file:
-                if x == 'k':
-                    all_text_liste_k.append(file.read())
-                elif x == 'n':
-                    all_text_liste_n.append(file.read())
-                elif x == 'c':
-                    tag_liste_c.append(file.read())
+            with open(fullpath, 'r', encoding="utf-8") as file:
+                if x == 'c':
+                    try:
+                        tag_liste_c.append(file.read())
+                    except: pass    
+                else:
+                    all_text_liste.append(file.read())
         if len(tag_liste_c) != 0:
-            all_text_liste_c.append(" ".join(tag_liste_c).replace('.', ""))
-    if x == 'k':
-        return all_text_liste_k
-    elif x == 'n':
-        return all_text_liste_n 
-    elif x == 'c':
-        return all_text_liste_c 
+            all_text_liste.append(" ".join(tag_liste_c).replace('.', ""))
+    return all_text_liste 
  
-# Vorkrisenzeit
-all_text_string_k = " ".join(einlesen('k')).replace('.', "")
-Tupel_Bag_k = collections.Counter(all_text_string_k.split())
+# Vorkrisenzeit einlesen
 
-# Normalzeit
-all_text_string_n = " ".join(einlesen('n')).replace('.', "")
-Tupel_Bag_n = collections.Counter(all_text_string_n.split())
+Tupel_Bag_k = Counter(chain.from_iterable(map(str.split, einlesen('k')))).most_common()
 
-del all_text_string_k, all_text_string_n
+count = 0
+for index, tupel in enumerate(Tupel_Bag_k):
+    if tupel[1]>8000:
+        count += 1
+    elif tupel[1]==49:
+        Tupel_Bag_k = Tupel_Bag_k[count:index]
+        break
 
-# Differenz
-Tupel_Bag_k.subtract(Tupel_Bag_n)
-Tupel_Bag = Tupel_Bag_k.most_common()
+# Normalzeit einlesen
 
-del Tupel_Bag_k, Tupel_Bag_n
+Tupel_Bag_n = Counter(chain.from_iterable(map(str.split, einlesen('n')))).most_common()
 
-# Sentiment
-path_Senti_positive = dir_path + '/Senti/SentiWS_v2.0_Positive.txt'
-#path_Senti_negative = dir_path + '/Senti/SentiWS_v2.0_Negative.txt'
+count = 0
+for index, tupel in enumerate(Tupel_Bag_n):
+    if tupel[1]>8000:
+        count += 1
+    elif tupel[1]==49:
+        Tupel_Bag_n = Tupel_Bag_n[count:index]
+        break
 
-Senti_positive = []
-#Senti_negative = []
+# auf gleiche Länge bringen
 
-with open(path_Senti_positive, 'r', encoding="utf8") as file:
-    Senti_positive = file.read().splitlines()
+Tupel_Bag_k = Tupel_Bag_k[:len(Tupel_Bag_n)] 
 
-# with open(path_Senti_negative, 'r', encoding="utf8") as file:
-#     Senti_negative = file.read().splitlines()
-    
-del path_Senti_positive
+# TTF zu Panda
+ttf_vorkrisenzeit = pd.DataFrame(list(Tupel_Bag_k), columns=['Wort', 'Anzahl'])
+ttf_normalzeit = pd.DataFrame(list(Tupel_Bag_n), columns=['Wort', 'Anzahl'])
 
-for index,element in enumerate(Senti_positive):
-    m = element.index('|')    
-    Senti_positive[index] = element[:m]
+# Gewichtung hinzufügen
+ttf_vorkrisenzeit['Gewichtung'] = ttf_vorkrisenzeit['Anzahl']/sum(ttf_vorkrisenzeit['Anzahl'])
+ttf_normalzeit['Gewichtung'] = ttf_normalzeit['Anzahl']/sum(ttf_normalzeit['Anzahl'])
 
-# for index,element in enumerate(Senti_negative):
-#     m = element.index('|')    
-#     Senti_negative[index] = element[:m]
+# Corona einlesen
+#tag_l = einlesen('c')   
 
-del m, index
+del index, count, tupel, Tupel_Bag_k, Tupel_Bag_n
 
-Tupel_Bag = Tupel_Bag[3:]
 
-for tupel in Tupel_Bag:
-    okay = True
-    for word in Senti_positive:
-        if word==tupel[0]:
-            Tupel_Bag.remove(tupel)
-            okay = False
-            break
-    if okay:    
-        if tupel[1] >= 20:       
-            Word_Bag.append(tupel[0])        
-            Anzahl_Bag.append(tupel[1])
-                
-        else:           
-            Bin_Bag.append(tupel[0])
+#%%
+# DESKRIPTOREN ERSTELLEN/EINLESEN
 
-## %%
-# Corona
-
-desc_l = []
-tag_l = einlesen('c')
-
-for tag in tag_l:
- 
-    l = tag.split()
-    desc = [0] * len(Word_Bag) 
-    for i, word in enumerate(Word_Bag):
-        for x in l:
-            if word == x:
-                desc[i] += 1
-    desc_l.append(desc)
-
-## %%
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy import sparse
-#import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 
-desc_l.insert(0, Anzahl_Bag)
-matrix = np.array(desc_l)  
+# EINLESEN
+deskriptoren_vorkrise = np.loadtxt('Matrix_Vorkrise_komp.txt', dtype=float)
+deskriptoren_normal = np.loadtxt('Matrix_Normal_komp.txt', dtype=float)
 
-similarities = cosine_similarity(sparse.csr_matrix(matrix))
+# ERSTELLEN
+# deskriptoren_vorkrise = np.zeros((len(tag_l), len(ttf_vorkrisenzeit)))
+# deskriptoren_normal = np.zeros((len(tag_l), len(ttf_normalzeit)))
 
-y = list(similarities[:,0])
-y = np.array(y[-len(tag_l):])
-x = np.array(list(range(len(tag_l))))
+# for i, tag in enumerate(tag_l):
+#     for wort in tag.split():
+#         for j, term in enumerate(ttf_normalzeit['Wort']):
+#             if wort == term:
+#                 deskriptoren_normal[i][j] += 1
+#                 break
 
-d = {'x': x, 'y': y}
-df = pd.DataFrame(d)
-sns.set(style='darkgrid')
-sns.lineplot(x='x', y='y', data=df)
+# for i, tag in enumerate(tag_l):
+#     for wort in tag.split():
+#         for j, term in enumerate(ttf_vorkrisenzeit['Wort']):
+#             if wort == term:
+#                 deskriptoren_vorkrise[i][j] += 1
+#                 break
+
+# del wort, term, i, j, tag
+     
+#%%
+# SCORE BERECHNEN
+
+score_vorkrise = []
+score_normal = []
+
+for i in range(len(deskriptoren_vorkrise)):
+    score = sum(deskriptoren_vorkrise[i] * np.array(ttf_vorkrisenzeit['Gewichtung']))
+    score_vorkrise.append(score)
+
+for i in range(len(deskriptoren_normal)):
+    score = sum(deskriptoren_normal[i] * np.array(ttf_normalzeit['Gewichtung']))
+    score_normal.append(score)      
+
+del i, score
+
+score_df = pd.DataFrame({'Vorkrise': score_vorkrise, 'Normal': score_normal})     
+score_df['p(Vorkrise)'] = score_df['Vorkrise']/(score_df['Vorkrise']+score_df['Normal'])
+score_df['p(Normal)'] = score_df['Normal']/(score_df['Vorkrise']+score_df['Normal'])
+
+score_df['Kommt Krise?'] = np.where((score_df['p(Vorkrise)'] > score_df['p(Normal)']), 1, 0)
+
+del score_normal, score_vorkrise
+
+#%%
+# DARSTELLUNG
+
+import matplotlib.pyplot as plt
+
+score_df.plot(kind='line',y='p(Vorkrise)', color='red', ax=plt.gca())
+score_df.plot(kind='line',y='p(Normal)', ax=plt.gca())
+
+plt.show()
+#%%
+# MATRIZEN SPEICHERN
+
+# np.savetxt('Matrix_Vorkrise_komp.txt', deskriptoren_vorkrise, fmt='%d')
+# np.savetxt('Matrix_Normal_komp.txt', deskriptoren_normal, fmt='%d')
